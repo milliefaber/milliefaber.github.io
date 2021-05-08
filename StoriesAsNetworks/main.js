0fe38e2623def1657d96244c03be8e25fee71c21
milliefaber <fabem974@newschool.edu>
2021-05-08 15:59:08 -0400

    Add files via upload

---
+++ StoriesAsNetworks/main.js
@@ -0,0 +1,113 @@
+const cursor = document.querySelector(".cursor")
+
+document.addEventListener('mousemove', e => {
+    console.log(e);
+    cursor.setAttribute("style", "top: "+(e.pageY - 100)+"px; left:  "+(e.pageX - 100)+"px;")
+})
+
+function update(e){
+    var x = e.clientX || e.touches[0].clientX
+    var y = e.clientY || e.touches[0].clientY
+  
+    document.documentElement.style.setProperty('--cursorX', x + 'px')
+    document.documentElement.style.setProperty('--cursorY', y + 'px')
+  }
+  
+  document.addEventListener('mousemove',update)
+  document.addEventListener('touchmove',update)
+
+// dots is an array of Dot objects,
+// mouse is an object used to track the X and Y position
+   // of the mouse, set with a mousemove event listener below
+   var dots = [],
+   mouse = {
+     x: 0,
+     y: 0
+   };
+
+// The Dot object used to scaffold the dots
+var Dot = function() {
+ this.x = 0;
+ this.y = 0;
+ this.node = (function(){
+   var n = document.createElement("div");
+   n.className = "trail";
+   document.body.appendChild(n);
+   return n;
+ }());
+};
+// The Dot.prototype.draw() method sets the position of 
+ // the object's <div> node
+Dot.prototype.draw = function() {
+ this.node.style.left = this.x + "px";
+ this.node.style.top = this.y + "px";
+};
+
+// Creates the Dot objects, populates the dots array
+for (var i = 0; i < 1; i++) {
+ var d = new Dot();
+ dots.push(d);
+}
+
+// This is the screen redraw function
+function draw() {
+ // Make sure the mouse position is set everytime
+   // draw() is called.
+ var x = mouse.x,
+     y = mouse.y;
+ 
+ // This loop is where all the 90s magic happens
+ dots.forEach(function(dot, index, dots) {
+   var nextDot = dots[index + 1] || dots[0];
+   
+   dot.x = x;
+   dot.y = y;
+   dot.draw();
+   x += (nextDot.x - dot.x) * .75;
+   y += (nextDot.y - dot.y) * .75;
+
+ });
+}
+
+addEventListener("mousemove", function(event) {
+ //event.preventDefault();
+ mouse.x = event.pageX - 200;
+ mouse.y = event.pageY - 200;
+});
+
+// animate() calls draw() then recursively calls itself
+ // everytime the screen repaints via requestAnimationFrame().
+function animate() {
+ draw();
+ requestAnimationFrame(animate);
+}
+
+// And get it started by calling animate().
+animate();
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
